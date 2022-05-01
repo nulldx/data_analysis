c972daebeef44d94616be36771332e926217f888
nulldx <2523654594@qq.com>
2022-05-01 16:31:16 +0800

    样式修改

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 
 squares = [1,4,9,16,25]
+input_values = [1,2,3,4,5]
 fig,ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares, linewidth=3)
+ax.set_title("平方数", fontsize=24,fontproperties="SimHei")
+ax.set_xlabel("值",fontsize=14,fontproperties="SimHei")
+ax.set_ylabel("值的平分", fontsize=14,fontproperties="SimHei")
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
